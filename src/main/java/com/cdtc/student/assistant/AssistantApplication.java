21b7d82584aa03e2867dc22ea5b3c80e94fcb02c
彭陈 <1176262251@qq.com>
2018-05-10 23:38:31 +0800

    配置Druid监控页面

--- src/main/java/com/cdtc/student/assistant/AssistantApplication.java
+++ src/main/java/com/cdtc/student/assistant/AssistantApplication.java
@@ -2,7 +2,11 @@ package com.cdtc.student.assistant;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+
 @SpringBootApplication
+@ServletComponentScan(value = "com.cdtc.student.assistant.servlet")
 public class AssistantApplication{
 
 
